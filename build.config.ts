586f05b58f2021560dc1c19ad558b5cbda18b41f
黄明富 <212149997@qq.com>
2025-04-09 14:21:52 +0800

    chore: 添加 build.config 注释

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,19 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-    entries: [
-        "./src/index",
-    ],
+    // 配置入口文件，指定需要打包的模块
+    entries: ["src/index",],
     rollup: {
+        // 启用 CommonJS 格式输出
         emitCJS: true,
+        // esbuild: {
+        //     minify: true, // 开启代码压缩
+        // },
     },
+    // 生成 TypeScript 类型声明文件
     declaration: true,
+    // 在构建前清理输出目录
+    clean: true,
+    // 构建过程中忽略警告，不因警告导致构建失败
     failOnWarn: false,
 });
